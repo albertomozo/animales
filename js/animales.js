5382f342ce09f05c1eca56090a5faa215fa00426
Alberto <albertomozodocente@gmail.com>
2024-03-28 10:24:03 +0100

    ficha animales back

--- js/animales.js
+++ js/animales.js
@@ -13,3 +13,31 @@ function getAnimalIcono(id) {
         return 'piggy-bank-svgrepo-com.svg';
     }
 }
+
+function getAnimalAlbum(id){
+    const albumes = JSON.parse(localStorage.getItem('album'));
+    var respuesta = new Array();
+    for (const album of albumes)
+    {
+         console.log(album);
+         const imagenesAlbum  = album.imagenes.filter(imagen => imagen.animalId === id);
+         //console.log(imagenesAlbum);
+         if (imagenesAlbum){
+         for (const imagenAlbum of imagenesAlbum)
+         {
+            console.log(album.espacioId,album.titulo);
+            //console.log(imagenAlbum);
+            console.log(imagenAlbum.foto);
+            let imagenAlbum2=imagenAlbum;
+            imagenAlbum2.espacioId = album.espacioId;
+            imagenAlbum2.titulo = album.titulo;
+            respuesta.push(imagenAlbum2);
+         }
+        }
+       
+         
+    }
+    return respuesta;
+   
+
+}
